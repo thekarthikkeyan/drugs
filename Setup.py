237a6dacdbe6b2ba3459588f8c1ed5e43ab0c256
thekarthikkeyan <85353666+thekarthikkeyan@users.noreply.github.com>
2023-01-04 22:04:42 -0800

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,7 +1,7 @@
 class wow:
     #i'm not going through basics
-    api_id = 123456 # your API_ID
-    api_hash = "" #ypur API_HASH
+    api_id = 26256222 # your API_ID
+    api_hash = "a828b6136f7d03990632a6bc14a8177d" 
     bot_token = "5888826999:AAFfb99Dgc1IKuoRY_BP-ExCp2cPy5qfMYE"
     openai_api_key = "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"
     model = "text-davinci-003" # use any of these [text-davinci-002,text-davinci-001]
